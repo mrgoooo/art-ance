3fe53c3bfb795f93ad205b029c02299025124659
Mrgo producent <mrgogames1@gmail.com>
2025-04-23 13:56:30 +0200

    cq

--- src/WebDev/WebDev.jsx
+++ src/WebDev/WebDev.jsx
@@ -1,5 +1,5 @@
 import WDHero from "./WDHero";
-import Navbar from "../Components/NavBar";
+import Navbar from "../Components/Navbar";
 import LastFooter from "../Components/LastFooter";
 import WDwhy from "./WDWhy";
 import WDMot from "./WDMot";
